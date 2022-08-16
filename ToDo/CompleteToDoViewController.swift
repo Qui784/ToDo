0fcf16c0c1440cb75c9f442234381cd09e55983b
Quianna Numa <quiannanuma@icloud.com>
2022-08-16 13:30:23 -0400

    Complete Iteration 2

--- ToDo/CompleteToDoViewController.swift
+++ ToDo/CompleteToDoViewController.swift
@@ -12,17 +12,22 @@ class CompleteToDoViewController: UIViewController {
     @IBOutlet weak var titleLabel: UILabel!
     
     var previousVC = ToDoTableViewController()
-    var selectedToDo = ToDo()
+    var selectedToDo : ToDoCD?
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        titleLabel.text = selectedToDo.name
+        titleLabel.text = selectedToDo?.name
     }
     
     @IBAction func completeTapped(_ sender: Any) {
-        
+        if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+            if let theToDo = selectedToDo {
+                context.delete(theToDo)
+                navigationController?.popViewController(animated: true)
+            }
+        }
     }
     
 
--- ToDo/ToDoTableViewController.swift
+++ ToDo/ToDoTableViewController.swift
@@ -9,27 +9,40 @@ import UIKit
 
 class ToDoTableViewController: UITableViewController {
     
-    var toDos : [ToDo] = []
+    var toDos : [ToDoCD] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        toDos = createToDos()
+        // getToDos()
 
     }
     
-    func createToDos() -> [ToDo] {
+    func getToDos() {
+        if let context = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
 
-        let swift = ToDo()
-        swift.name = "Learn Swift"
-        swift.important = true
-
-        let dog = ToDo()
-        dog.name = "Walk the Dog"
-        
-        // important is set to false by default
-
-        return [swift, dog]
+            if let coreDataToDos = try? context.fetch(ToDoCD.fetchRequest()) as? [ToDoCD] {
+                toDos = coreDataToDos
+                tableView.reloadData()
+            }
+        }
+    }
+    
+//    func createToDos() -> [ToDo] {
+//
+//        let swift = ToDo()
+//        swift.name = "Learn Swift"
+//        swift.important = true
+//
+//        let dog = ToDo()
+//        dog.name = "Walk the Dog"
+//
+//        // important is set to false by default
+//
+//        return [swift, dog]
+//    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        getToDos()
     }
     
 
@@ -46,14 +59,16 @@ class ToDoTableViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
 
         let toDo = toDos[indexPath.row]
-        
-        if toDo.important {
-            cell.textLabel?.text = "❗️" + toDo.name
-          } else {
-            cell.textLabel?.text = toDo.name
-          }
-        
-        return cell
+
+        if let name = toDo.name {
+            if toDo.important {
+                cell.textLabel?.text = "❗️" + name
+            } else {
+                cell.textLabel?.text = toDo.name
+            }
+        }
+
+      return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -69,12 +84,12 @@ class ToDoTableViewController: UITableViewController {
         if let addVC = segue.destination as? AddToDoViewController {
             addVC.previousVC = self
         }
-    }
-    
-    if let completeVC = segue.destination as? CompleteToDoViewController {
-        if let toDo = sender as? ToDo {
-            completeVC.selectedToDo = toDo
-            completeVC.previousVC = self
+
+        if let completeVC = segue.destination as? CompleteToDoViewController {
+            if let toDo = sender as? ToDoCD {
+                completeVC.selectedToDo = toDo
+                completeVC.previousVC = self
+            }
         }
     }
 
